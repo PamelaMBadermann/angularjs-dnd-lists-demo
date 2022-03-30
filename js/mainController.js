c4c1daad65d8483e5ddc44a405fca50503a2a7a5
Pamela Badermann <badermann.pamela@gmail.com>
2022-03-30 14:01:59 -0300

    adjusting dnd-lists styles

--- js/mainController.js
+++ js/mainController.js
@@ -1,11 +1,36 @@
-angular.module("dragAndDrop", ["ngMessages"]);
+angular.module("dragAndDrop", ["dndLists"]);
 angular.module("dragAndDrop").controller("mainController", function ($scope,) {
     $scope.app = "AngularJS drag and drop lists";
 
     $scope.items = [
-        { text: "Lorem ipsum dolor sit amet, consectetur adip minim veniam nulla pariatur auctor." },
-        { text: "Lorem ipsum dolor sit amet, consectetur adip minim veniam nulla pariatur auctor." },
-        { text: "Lorem ipsum dolor sit amet, consectetur adip minim veniam nulla pariatur auctor." },
-        { text: "Lorem ipsum dolor sit amet, consectetur adip minim veniam nulla pariatur auctor." },
+        {
+            selected: null,
+            text: "AAA Lorem ipsum dolor sit amet, consectetur adip minim veniam nulla pariatur auctor."
+        },
+        {
+            selected: null,
+            text: "BBB Lorem ipsum dolor sit amet, consectetur adip minim veniam nulla pariatur auctor."
+        },
+        {
+            selected: null,
+            text: "CCC Lorem ipsum dolor sit amet, consectetur adip minim veniam nulla pariatur auctor."
+        },
+        {
+            selected: null,
+            text: "DDD Lorem ipsum dolor sit amet, consectetur adip minim veniam nulla pariatur auctor."
+        },
     ];
+
+    // $scope.dropCallback = function (event, index, item, external, type, allowedType) {
+    //     if (external) {
+    //         return false;
+    //     }
+    //     return item;
+    // };
+
+    // $scope.dropCallback = function(index, item, external, type) {
+    //     $scope.logListEvent('dropped at', index, external, type);
+    //     // Return false here to cancel drop. Return true if you insert the item yourself.
+    //     return item;
+    // };
 });
